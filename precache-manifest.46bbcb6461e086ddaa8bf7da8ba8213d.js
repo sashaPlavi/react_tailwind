cb9e9070747f2e49dc6692d12b6eafe8e2847edf
SashaPlavi <sasha.bingulac@gmail.com>
2020-04-18 21:28:49 +0200

    Updates

---
+++ precache-manifest.46bbcb6461e086ddaa8bf7da8ba8213d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb47ede922c87e6ff128bd19196747f8",
+    "url": "/sashaPlavi/react_tailwind/index.html"
+  },
+  {
+    "revision": "a6560abf3352cfe72dae",
+    "url": "/sashaPlavi/react_tailwind/static/css/main.40ec6352.chunk.css"
+  },
+  {
+    "revision": "ab91d8bdcf5298f7375a",
+    "url": "/sashaPlavi/react_tailwind/static/js/2.12108a10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sashaPlavi/react_tailwind/static/js/2.12108a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6560abf3352cfe72dae",
+    "url": "/sashaPlavi/react_tailwind/static/js/main.cfc65846.chunk.js"
+  },
+  {
+    "revision": "2cc7b666fc14d5a78334",
+    "url": "/sashaPlavi/react_tailwind/static/js/runtime-main.dbd03bf5.js"
+  }
+]);
